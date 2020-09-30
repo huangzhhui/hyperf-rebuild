1ca977854ec483ee16448bd19e02082dd072a3f2
hayden <mfkhao2009@outlook.com>
2020-09-30 12:28:02 +0800

    optimize

--- src/Command/StartCommand.php
+++ src/Command/StartCommand.php
@@ -30,14 +30,13 @@ class StartCommand extends Command
         $this->setName('start')->setDescription('启动服务');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
         $config = $this->config;
         $configs = $config->get('server');
         $serverFactory = new ServerFactory();
         $serverFactory->configure($configs);
         $serverFactory->start();
-        return 1;
     }
 
 
